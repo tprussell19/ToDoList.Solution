94b734002af51373bd26a6f1ccd917313476b0dd
Thomas Russell <80493022+tprussell19@users.noreply.github.com>
2021-05-04 20:50:51 -0700

    Initial commit

---
+++ Template.Tests/ModelTests/ProgramTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Template.Models;
+
+namespace Template.TestTools
+{
+  [TestClass]
+  public class Program
+  {
+    //HERE IS WHERE YOU NEED TO PASS YOUR TESTS INTO.
+  }
+}
